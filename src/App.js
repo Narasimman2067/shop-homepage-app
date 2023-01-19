0e04b2bf9f06cbcc48c0186e361e1d95a1d5344c
Narasimman2067 <narasimmamech2067@gmail.com>
2023-01-19 23:19:19 +0530

    shop homepage app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,170 @@
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <ShopCard1/>
+   <ShopCard2/>
+   <ShopCard3/>
+   <ShopCard4/>
+   <ShopCard5/>
+   <ShopCard6/>
+   <ShopCard7/>
+   <ShopCard8/>
     </div>
   );
 }
 
 export default App;
+
+
+
+
+function ShopCard1() {
+  return (
+    <div className="App">
+   <nav className="numsize">
+<p>450 x 300</p>
+<h1>Fancy Product</h1>
+<h3>$40.00 - $80.00</h3>
+<a href=""><button className="switch">view options</button></a>
+
+  </nav>
+
+    </div>
+  );
+}
+function ShopCard2() {
+  return (
+    <div className="App">
+   <nav className="numsize">
+    <p>450 x 300</p>
+    <h1>Special Item</h1>
+    <span className="star">⭐⭐⭐⭐⭐</span>
+    <h3><span className="s">$20.00</span> $18.00</h3>
+    <a href=""><button>add to cart</button></a>
+
+  </nav>
+
+    </div>
+  );
+}
+function ShopCard3() {
+  return (
+    <div className="App">
+   <nav className="numsize">
+<p>450 x 300</p>
+<h1>Sale Item</h1>
+<h3><span className="s">$50.00</span> $25.00</h3>
+<a href=""><button >add to cart</button></a>
+
+  </nav>
+
+    </div>
+  );
+}
+
+function ShopCard4() {
+  return (
+    <div className="App">
+   <nav className="numsize">
+<p>450 x 300</p>
+<h1>Popular Item</h1>
+<span className="star">⭐⭐⭐⭐⭐</span>
+<h3>$40.00</h3>
+<a href=""><button>add to cart</button></a>
+
+  </nav>
+
+    </div>
+  );
+}
+function ShopCard5() {
+  return (
+    <div className="App">
+   <nav className="numsize">
+<p>450 x 300</p>
+<h1>Sale Item</h1>
+<h3><span className="s">$50.00</span> $25.00</h3>
+<a href=""><button className="switch">add to cart</button></a>
+
+  </nav>
+
+    </div>
+  );
+}
+function ShopCard6() {
+  return (
+    <div className="App">
+   <nav className="numsize">
+<p>450 x 300</p>
+<h1>Fancy Product</h1>
+<h3><span className="s">$120.00</span> $280.00</h3>
+<a href=""><button className="switch">view options</button></a>
+
+  </nav>
+
+    </div>
+  );
+}
+
+function ShopCard7() {
+  return (
+    <div className="App">
+   <nav className="numsize">
+<p>450 x 300</p>
+<h1>Special Item</h1>
+<span className="star">⭐⭐⭐⭐⭐</span>
+<h3><span className="s">$20.00</span> $18.00</h3>
+<a href=""><button>add to cart</button></a>
+
+  </nav>
+
+    </div>
+  );
+}
+
+function ShopCard8() {
+  return (
+    <div className="App">
+   <nav className="numsize">
+<p>450 x 300</p>
+<h1>Popular Item</h1>
+<span className="star">⭐⭐⭐⭐⭐</span>
+<h3>$40.00</h3>
+<a href=""><button>add to cart</button></a>
+
+  </nav>
+
+    </div>
+  );
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
